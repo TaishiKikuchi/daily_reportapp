f30579672775ac8dfd471f867e58f8779d10a076
Taishi <taishi.kikuchi@e-grant.net>
2019-04-18 16:21:48 +0900

    [update]日報一覧を部署ごとに表示できるように[fix]作業内容更新時に重複して作業内容が追加されないように

--- app/Model/Report.php
+++ app/Model/Report.php
@@ -1,30 +1,26 @@
 <?php
 
-class Report extends AppModel {
-    public function isOwnedBy($report, $user) {
+class Report extends AppModel
+{
+    public function isOwnedBy($report, $user)
+    {
         return $this->field('id', array('id' => $report, 'user' => $user)) !== false;
     }
-    /*
-    public function getReport($id){
-        $sql = "SELECT * FROME sample WHERE sample.id = :id;";
 
-        $params = array(
-            'id'=> $id
-        );
-
-        $report = $this->query($sql,$params);
-        return $report;
-    }
-    */
     public $hasMany = array(
         'Work' => array(
             'className' => 'Work',
-            //'conditions' => array('Work.report_id' => ''),
             'order' => 'Work.starttime ASC'
         ),
         'Share' => array(
-            'className' => 'Share',
-            //'conditions' => array('Share.report_id' => ''),
+            'className' => 'Share'
+        )
+    );
+    
+    public $belongsTo = array(
+        'User' => array(
+            'className' => 'User',
+            'foreignKey' => 'user_id'
         )
     );
 }
